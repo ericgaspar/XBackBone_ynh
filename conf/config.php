b4a0fca1597414099463bddd6863d9f8d12d1635
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2022-11-30 20:01:26 +0100

    Update config.php

--- conf/config.php
+++ conf/config.php
@@ -4,12 +4,12 @@ return [
     'base_url' => 'https://__DOMAIN__/__PATH__', // no trailing slash
     'db'       => [
         'connection' => 'sqlite',
-        'dsn'        => realpath(__DIR__).'/resources/database/xbackbone.db',
+        'dsn'        => '/resources/database/xbackbone.db',
         'username'   => null,
         'password'   => null,
     ],
     'storage' => [
         'driver' => 'local',
-        'path'   => realpath(__DIR__).'__DATADIR__',
+        'path'   => '__DATADIR__',
     ],
 ];

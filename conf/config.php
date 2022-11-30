530c3aeacefe2824866b77482b81d56dde12bfce
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2022-11-30 19:57:21 +0100

    Revert "Fix"
    This reverts commit 88eb440cb384b5b74c31ba9266c1638a92b5b8cb.

--- conf/config.php
+++ conf/config.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'base_url' => 'https://__DOMAIN__', // no trailing slash
+    'base_url' => 'https://__DOMAIN__/__PATH__', // no trailing slash
     'db'       => [
         'connection' => 'mysql',
-        'dsn'        => 'host=127.0.0.1;port=3306;dbname=__DB_NAME__',
+        'dsn'        => 'host=localhost;port=3306;dbname=__DB_NAME__',
         'username'   => '__DB_USER__',
         'password'   => '__DB_PWD__',
     ],
